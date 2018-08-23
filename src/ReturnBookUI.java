193a41146bc64956fa5cec90bf12a271b222b93a
professionalprogramming <41613977+professionalprogramming@users.noreply.github.com>
2018-08-23 19:31:36 +1000

    ReturnBookUI.java
    // Change the place {

--- src/ReturnBookUI.java
+++ src/ReturnBookUI.java
@@ -3,43 +3,50 @@ import java.util.Scanner;
 
 public class ReturnBookUI {
 
-	public static enum UiState { INITIALISED, READY, INSPECTING, COMPLETED }; //Reaname UT_STAE to UiState
+	public static enum UiState  (INITIALISED, READY, INSPECTING, COMPLETED) ; //Reaname UT_STAE to UiState and change the { to (
 
 	private ReturnBookControl control;
 	private static Scanner scanner = new Scanner( System.in ); // change the scanner input.
 	private UiState state; // Reaname UT_STAE to UiState
 
 	
-	public ReturnBookUI(ReturnBookControl control) {
+	public ReturnBookUI(ReturnBookControl control)
+	{ 							// Change the place {
 		this.control = control;
-		Scanner scanner = new Scanner( System.in ); // change the scanner input..
+		Scanner scanner = new Scanner( System.in ); // Change the scanner input..
 		
-		state = UiState.INITIALISED; // Reaname UT_STAE to UiState
+		state = UiState.INITIALISED;                // Reaname UT_STAE to UiState
 		control.setUI(this);
 	}
 
 
-	public void run() {		
+	public void run() 
+	{							      // Change the place {
 		    System.out.println("Return Book Use Case UI\n"); // Change the output to  System.out.println.
 		
-		while (true) {
+		while (true) 
+		{
 			
-			switch (state) {
+			switch (state) 
+			{
 			
 			case INITIALISED:
 			break;
 				
 			case READY:
 				String bookStr = input("Scan Book (<enter> completes): ");
-				if (bookStr.length() == 0) {
+				if (bookStr.length() == 0) 
+				{
 					control.scanningComplete();
 				}
 				else {
-					try {
+					try 
+					{
 						int bookId = Integer.valueOf(bookStr).intValue();
 						control.bookScanned(bookId);
 					}
-					catch (NumberFormatException e) {
+					catch (NumberFormatException e) 
+					{
 						System.out.println("Invalid bookId"); // Change the output to  System.out.println.
 					}					
 				}
@@ -48,7 +55,8 @@ public class ReturnBookUI {
 			case INSPECTING:
 				String ans = input("Is book damaged? (Y/N): ");
 				boolean isDamaged = false;
-				if (ans.toUpperCase().equals("Y")) {					
+				if (ans.toUpperCase().equals("Y")) 
+				{					
 					isDamaged = true;
 				}
 				control.dischargeLoan(isDamaged);
@@ -65,18 +73,21 @@ public class ReturnBookUI {
 	}
 
 	
-	private String input(String prompt) {
+	private String input(String prompt)
+	{
 		System.out.print(prompt);
 		return input.nextLine();
 	}	
 		
 		
-	private void output(Object object) {
+	private void output(Object object) 
+	{
 		System.out.println(object);
 	}
 	
 			
-	public void display(Object object) {
+	public void display(Object object) 
+	{
 		output(object);
 	}
 	
