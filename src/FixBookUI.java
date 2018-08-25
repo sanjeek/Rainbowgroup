92f63e7db8541026a201a242881addc71680007b
Sanjeevan <sanjee@live.com>
2018-08-25 22:52:45 +1000

    Update FixBookUI.java
    fixed

--- src/FixBookUI.java
+++ src/FixBookUI.java
@@ -80,7 +80,7 @@ public class FixBookUI {
 
 	
 	private String input(String prompt) {
-		System.out.print(prompt);
+		System.out.println(prompt);//change to println
 		return input.nextLine();
 	}	
 		
